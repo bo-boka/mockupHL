a33697d9f171f32e11cff6bb28ae491958e2fea9
sarahboka <boka.sarah@gmail.com>
2018-02-08 17:04:08 -0500

    added angular and all html pages

--- js/script.js
+++ js/script.js
@@ -5,39 +5,61 @@ myApp.config(function ($routeProvider) {
   $routeProvider
 
   .when('/', {
-    templateUrl: './pages/main.html',
+    templateUrl: '/pages/main.html',
     controller: 'mainController'
   })
 
-  .when('/second', {
-    templateUrl: './pages/second.html',
+  .when('/service', {
+    templateUrl: '/pages/service.html',
     controller: 'secondController'
   })
 
-  .when('/third', {
-    templateUrl: './pages/third.html',
+  .when('/portfolio', {
+    templateUrl: '/pages/portfolio.html',
     controller: 'thirdController'
   })
 
-  .when('/fourth', {
-    templateUrl: './pages/fourth.html',
+  .when('/team', {
+    templateUrl: '/pages/team.html',
     controller: 'fourthController'
   })
 
+  .when('/blog', {
+    templateUrl: '/pages/blog.html',
+    controller: 'fifthController'
+  })
+
+
 });
 
-myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+myApp.controller('mainController', ['$scope', '$location', '$log', function($scope, $location, $log) {
+
+  $log.info($location.path());
+
+  $scope.name = 'Main';
 
 }]);
 
 myApp.controller('secondController', ['$scope', '$log', function($scope, $log) {
 
+  $scope.name = 'Service';
+
 }]);
 
 myApp.controller('thirdController', ['$scope', '$log', function($scope, $log) {
 
+  $scope.name = 'Portfolio';
+
 }]);
 
 myApp.controller('fourthController', ['$scope', '$log', function($scope, $log) {
+
+  $scope.name = 'Team';
+
+}]);
+
+myApp.controller('fifthController', ['$scope', '$log', function($scope, $log) {
+
+  $scope.name = 'Blog';
 
 }]);
